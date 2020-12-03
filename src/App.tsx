3e9f671a80c79d11eb6dc62357eb424f3cb76dd3
Cyril <cdsouza@boardera.ca>
2020-12-02 19:43:52 -0500

    added header page and style

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, Switch } from  'react-router-dom';
 import HomePage from './pages/homePage/homePage';
 import ShopPage from './pages/shopPage';
+import Header from './components/header';
 
 const  App: React.FunctionComponent = () => (
     <div>
+        <Header />
         <Switch>
             <Route exact path="/" component={HomePage}/>
             <Route exact path="/shop" component={ShopPage}/>
