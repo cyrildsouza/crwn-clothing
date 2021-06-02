bca07e84ff3432d63eab7010957334a728a0039c
Cyril <cdsouza@boardera.ca>
2021-06-02 18:25:41 -0400

    removed logger from redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,8 +3,10 @@ import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import reducer from './reducer';
 
-const middlewares = [logger];
+const middlewares = [];
 
+if (process.env.NODE_ENV === 'development')
+    middlewares.push(logger);
 const store = createStore(reducer, applyMiddleware(...middlewares));
 
 const  persistor = persistStore(store);
